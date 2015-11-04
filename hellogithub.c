91731f3eb3b1048815fb2c293d9b076fa6af3e16
pipogy <1003621285@qq.com>
2015-11-04 13:55:53 +0800

    Update hellogithub.c for adapt in using github !
    add a function addsome in main !

--- hellogithub.c
+++ hellogithub.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
+void addsome(void);
 void main (void)
 {
-
+  addsome();
   printf("hello github.com !\n");
 
+}
+void addsome(void)
+{
+  printf("add some new thing !\n");
+  
 }
